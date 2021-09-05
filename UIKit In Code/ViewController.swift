3f756fde9b3c135d5f083a0879e0b85b1f075475
javierCueto <javelie@outlook.com>
2021-09-05 09:37:14 -0600

    Initial Commit

---
+++ UIKit In Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit In Code
+//
+//  Created by Javier Cueto on 05/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
